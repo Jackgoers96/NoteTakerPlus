f2979c720dbb391dcb0268a5741a5214351d6a16
Jackgoers96 <81663225+Jackgoers96@users.noreply.github.com>
2021-09-24 13:10:15 -0500

    Got it

--- Develop/fileSystem/saver.js
+++ Develop/fileSystem/saver.js
@@ -3,15 +3,16 @@ const util = require('util');
 
 // Promise version of fs.readFile
 const readFromFile = util.promisify(fs.readFile);
-const readingData = () => {
-  return  readFromFile("./db/db.json", 'utf-8'); 
-}
-
-const writeToFile = (destination, content) => {
+/**
+ *  Function to write data to the JSON file given a destination and some content
+ *  @param {string} destination The file you want to write to.
+ *  @param {object} content The content you want to write to the file.
+ *  @returns {void} Nothing
+ */
+const writeToFile = (destination, content) =>
   fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
     err ? console.error(err) : console.info(`\nData written to ${destination}`)
   );
-};
 /**
  *  Function to read data from a given a file and append some content
  *  @param {object} content The content you want to append to the file.
@@ -30,4 +31,4 @@ const readAndAppend = (content, file) => {
   });
 };
 
-module.exports = { readFromFile, writeToFile, readAndAppend , readingData};
+module.exports = { readFromFile, writeToFile, readAndAppend };
--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,60 +1,49 @@
-let uniqid = require('uniqid');
-const {
-    writeToFile,
-  } = require("../fileSystem/saver");
-  console.log(writeToFile);
-  const fs = require("fs");
-  
-  module.exports = function (app) {
-    app.get("/api/notes", (req, res) => {
-      //Reading data from JSON file using Fs module readFile()
-      fs.readFile("./db/db.json", "utf8", (err, data) => {
-        if (err) {
-          console.error(err);
+const notes = require('../db/db.json');
+const { readFromFile, readAndAppend, writeToFile } = require('../fileSystem/saver');
+const uniqid = require('uniqid');
+
+
+module.exports = (app) => {
+    app.get('/api/notes', (req, res) => {
+        readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+    })
+
+    app.post('/api/notes', (req, res) => {
+
+        const {title, text} = req.body;
+
+        if (title && text) {
+            const newNote = {
+                title,
+                text,
+                id: uniqid(),
+            };
+
+            readAndAppend(newNote, './db/db.json');
+
+            const response = {
+                status: 'success',
+                body: newNote,
+            };
+            console.log(response);
+            res.json(response);
         } else {
-          //Parsing data in JSON object to display on the HTML, in case of no values - assign a blank array
-          let storeNotes = JSON.parse(data);
-          //Returning the parsed object to the HTML page
-          res.json(storeNotes);
+            res.json('Error in posting new Note');
         }
-      });
     });
-  
-    app.post("/api/notes", (req, res) => {
-      //Reading data from JSON file using Fs module readFile()
-      fs.readFile("./db/db.json", "utf8"); {
-          let storeNotes = JSON.parse(data) || [];
-          req.body.id = uniqid()
-          storeNotes.push(req.body);
-          //Updating the JSON file with new combined Notes
-          writeToFile("./db/db.json", storeNotes) 
-          //   if (err) throw err;
-          // });
-          //Return JSON object to the display HTML.
-          res.json(storeNotes);
+
+    app.delete('/api/notes/:id', (req, res) => {
+
+        const {id} = req.params;
+
+        for (var i = 0; i < notes.length; i++) {
+            if (notes[i].id ==id) {
+                notes.splice(i,1);
+                break;
+            }
         }
-      });
-    };
-  
-//     //To delete notes
-//     app.delete("/api/notes/:id", (req, res) => {
-//       //Converting params into an integer 
-//       console.log(typeof req.params.id)
-//       let deletedNote = req.params.id;
-//       let newStoredNotes = [];
-//       fs.readFile("./db/db.json", "utf8", (err, data) => {
-//         //Refreshing values
-//         storeNotes = [].concat(JSON.parse(data));
-//         for (let i = 0; i < storeNotes.length; i++) {
-//           //Filtering out the delted note from the Note  list
-//           if (deletedNote !== storeNotes[i].id) {
-//             newStoredNotes.push(storeNotes[i]);
-//           }
-//         }
-//         //Updating the JSON file with the filtered Data
-//         writeToFile("./db/db.json", newStoredNotes)
-//         //Returning the updated JSON object to hTML page
-//         res.json(newStoredNotes);
-//       });
-//     });
-//   ;
+
+        writeToFile('./db/db.json', notes);
+        res.json('Successfully deleted note!')
+    })
+}
